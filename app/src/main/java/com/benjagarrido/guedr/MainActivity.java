6c19b6105b6e9547272c0c8594f2e512f4bef0f6
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-11-07 10:45:45 +0100

    bgarrido - adding a new button

--- app/src/main/java/com/benjagarrido/guedr/MainActivity.java
+++ app/src/main/java/com/benjagarrido/guedr/MainActivity.java
@@ -18,24 +18,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Asociamos controlador con vista a través del identificador
-        Button btnChangeSystem = (Button) findViewById(R.id.btn_change_system);
+        Button btnChangeToSpanishSystem = (Button) findViewById(R.id.btn_change_to_spanish_system);
+        Button btnChangeToAmericanSystem = (Button) findViewById(R.id.btn_change_to_american_system);
 
         // Accedemos a imageView a través de su id
         forecastImage = (ImageView) findViewById(R.id.forecast_image);
 
-        btnChangeSystem.setOnClickListener(new View.OnClickListener() {
+        btnChangeToSpanishSystem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(TAG,"Dentro del evento onClick del botón btn_change_system_button");
+                Log.v(TAG,"Dentro del evento onClick del botón btnChangeToSpanishSystem");
                 changeToSpanishSystem (v);
             }
         });
 
+        btnChangeToAmericanSystem.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG,"Dentro del evento onClick del botón btnChangeToAmericanSystem");
+                changeToAmericanSystem (v);
+            }
+        });
 
     }
     public void changeToSpanishSystem (View view){
         Log.v(TAG,"Dentro del método changeToSpanishSystem");
         // Cambiamos la imágen a mostrar
         forecastImage.setImageResource(R.drawable.offline_weather2);
+    }
+
+    public void changeToAmericanSystem (View view){
+        Log.v(TAG,"Dentro del método changeToAmericanSystem");
+        // Cambiamos la imágen a mostrar
+        forecastImage.setImageResource(R.drawable.offline_weather);
     }
 }
