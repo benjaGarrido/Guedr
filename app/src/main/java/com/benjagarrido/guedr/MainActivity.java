1e8ae16b399d150c6d7110c88895e39ef3343d13
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-11-07 19:43:56 +0100

    bgarrido - changing buttons by togglebutton control

--- app/src/main/java/com/benjagarrido/guedr/MainActivity.java
+++ app/src/main/java/com/benjagarrido/guedr/MainActivity.java
@@ -6,8 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
     private ImageView forecastImage;
     @Override
@@ -17,16 +18,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Cargamos la interfaz
         setContentView(R.layout.activity_main);
 
-        // Asociamos controlador con vista a través del identificador
-        Button btnChangeToSpanishSystem = (Button) findViewById(R.id.btn_change_to_spanish_system);
-        Button btnChangeToAmericanSystem = (Button) findViewById(R.id.btn_change_to_american_system);
-
         // Accedemos a imageView a través de su id
         forecastImage = (ImageView) findViewById(R.id.forecast_image);
-
-        btnChangeToSpanishSystem.setOnClickListener(this);
-        btnChangeToAmericanSystem.setOnClickListener(this);
-
+        // Asociamos el botón con el controlador
+        final ToggleButton btnForecastChange = (ToggleButton) findViewById(R.id.btn_forecast_change);
+        btnForecastChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnForecastChange.isChecked()){
+                    changeToSpanishSystem(v);
+                } else {
+                    changeToAmericanSystem(v);
+                }
+            }
+        });
     }
     public void changeToSpanishSystem (View view){
         Log.v(TAG,"Dentro del método changeToSpanishSystem");
@@ -38,17 +43,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.v(TAG,"Dentro del método changeToAmericanSystem");
         // Cambiamos la imágen a mostrar
         forecastImage.setImageResource(R.drawable.offline_weather);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_change_to_american_system:
-                changeToAmericanSystem(v);
-                break;
-            case R.id.btn_change_to_spanish_system:
-                changeToSpanishSystem(v);
-                break;
-        }
     }
 }
